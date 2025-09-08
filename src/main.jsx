46d5972c7d1faaaef1d4d2e39215ca5bf8bb7d36
wilmond-jpg <wilmondraylybarut19@gmail.com>
2025-09-08 23:30:46 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
-
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
+import { initAnalytics } from './analytics'  // <-- add this
+initAnalytics();                              // <-- and this
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
